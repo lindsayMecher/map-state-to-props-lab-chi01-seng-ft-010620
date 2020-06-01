4b0a05b392d747d4df9ccab7684eed292546cee0
Lindsay Mecher <lindsaymecher@gmail.com>
2020-06-01 17:06:01 -0500

    passed all tests

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
 
 class UserInput extends Component {
 
   state = {
     username: '',
-    hometown: ''
+    hometown: '',
+    doggo: ''
   }
 
+  // input is handled by the components local state which holds
+  // the entire state object. Then you can send that to the store.
   handleInputChange = (event) => {
     this.setState({
       [event.target.id]: event.target.value
@@ -15,6 +18,9 @@ class UserInput extends Component {
   }
 
   handleOnSubmit = (event) => {
+    // prevent default submission of form.
+    // dispatch the type and the payload to the store
+    // in this case the payload is action.user.
     event.preventDefault();
     this.props.dispatch({type: 'ADD_USER', user: this.state})
   }
@@ -36,6 +42,14 @@ class UserInput extends Component {
             id="hometown"
             onChange={this.handleInputChange}
             placeholder="hometown"
+          />
+        </p>
+        <p>
+        <input
+            type="text"
+            id="doggo"
+            onChange={this.handleInputChange}
+            placeholder="doggo"
           />
         </p>
         <input type="submit" />
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,21 +1,40 @@
 import React, { Component } from 'react';
-// add any needed imports here
+import { connect } from 'react-redux';
+
+
 class Users extends Component {
 
+  renderUsernames = () => {
+    return this.props.users.map((user, index) => {
+      return(
+        <li key={index}>
+          <h2>Name: {user.username}</h2>
+          <h2>Hometown: {user.hometown}</h2>
+          <h2>Doggo: {user.doggo}</h2>
+        </li>
+      )
+    })
+  }
+
   render() {
+    console.log(this.props)
+    console.log(this.props.users)
     return (
       <div>
         <ul>
           Users!
-          {/* Write code here that displays the usernames of all users in the Redux store */}
-          {/* In addition, display the total number of users curently in the store */}
+          {this.renderUsernames()}
+          Total number of users = {this.props.users.length}
         </ul>
       </div>
     )
   }
 }
 
-//add mapStateToProps here
+const mapStateToProps = (state) => {
+  return {
+    users: state.users
+  }
+}
 
-// connect this component to Redux
-export default Users
+export default connect(mapStateToProps)(Users)
